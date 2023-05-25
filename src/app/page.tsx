c3f3527877a0c1dfc5832cb073a5f891f36435e0
howooking <junsgk@gmail.com>
2023-05-25 11:28:39 +0900

    commit for CI/CD

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Carousel from '@/sections/a-carousel';
-import Salute from '@/sections/b-salute';
-import Subjects from '@/sections/c-subjects';
-import Staff from '@/sections/d-staff';
-import Facility from '@/sections/e-facility';
-import Map from '@/sections/f-map';
+import Carousel from '@/sections/carousel';
+import Salute from '@/sections/salute';
+import Subjects from '@/sections/subjects';
+import Staff from '@/sections/staff';
+import Facility from '@/sections/facility';
+import Map from '@/sections/map';
 
 export default function Home() {
   return (
