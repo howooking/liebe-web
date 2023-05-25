27022a73d295aef00bb61c79c0c63317dd2ba369
howooking <junsgk@gmail.com>
2023-05-25 11:37:57 +0900

    commit for CI/CD

--- src/sections/carousel.tsx
+++ src/sections/carousel.tsx
@@ -3,7 +3,7 @@ import Slides from '../components/slides';
 export default function Carousel() {
   return (
     <section className="h-screen overflow-hidden" id="carousel">
-      <Slides />
+      {/* <Slides /> */}
     </section>
   );
 }
